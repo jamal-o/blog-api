1353ab7209f903d0ddaa06ec3589a04b92dcb08d
jamal-o <jamaldeenolasupo@gmail.com>
2025-06-16 14:12:34 +0100

    build: update package.json

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ dotenv.config();
 database.connectDB();
 
 const port = process.env.PORT || 3000;
+const host = process.env.HOST || "0.0.0.0";
 
 app.listen(port, () => {
-	console.log(`Server started http://localhost:${port}`);
+	console.log(`Server started http://${host}:${port}`);
 });
